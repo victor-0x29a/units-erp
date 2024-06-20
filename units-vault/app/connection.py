aed844925094e69bcc386bccedec0f9aec9b18aa
Victor <cartoonbr495@gmail.com>
2024-06-20 08:30:45 -0400

    Chore: connecting into database with mongoengine and changing the host of mongodb

--- units-vault/app/connection.py
+++ units-vault/app/connection.py
@@ -1,4 +1,6 @@
 from pymongo import MongoClient
-from constants import MONGO_URI
+from mongoengine import connect
+from constants import MONGO_URI, MONGO_DATABASE, MONGO_HOST, MONGO_PASS, MONGO_USER
 
 mongo_client = MongoClient(MONGO_URI)
+connect(db=MONGO_DATABASE, host=MONGO_HOST, username=MONGO_USER, password=MONGO_PASS)
