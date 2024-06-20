a3474317991fc80cf9a6d9561485f89da23f563c
Victor <cartoonbr495@gmail.com>
2024-06-20 09:23:40 -0400

    Chore: correct connection for mongodb

--- units-vault/app/connection.py
+++ units-vault/app/connection.py
@@ -2,5 +2,12 @@ from pymongo import MongoClient
 from mongoengine import connect
 from constants import MONGO_URI, MONGO_DATABASE, MONGO_HOST, MONGO_PASS, MONGO_USER
 
+
 mongo_client = MongoClient(MONGO_URI)
-connect(db=MONGO_DATABASE, host=MONGO_HOST, username=MONGO_USER, password=MONGO_PASS)
+connect(
+    db=MONGO_DATABASE,
+    host=MONGO_HOST,
+    username=MONGO_USER,
+    password=MONGO_PASS,
+    authentication_source='admin'
+)
