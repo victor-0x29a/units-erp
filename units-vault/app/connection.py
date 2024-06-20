fd9bfe6b27eb80da9d2ff8c011b64122b4350e76
Victor <cartoonbr495@gmail.com>
2024-06-20 07:55:38 -0400

    Feat: necessary packages added and mongo connection created
    MongoEngine and pymongo added, and all necessary environment vars
    exported from app/constants.py for connection

---
+++ units-vault/app/connection.py
@@ -0,0 +1,4 @@
+from pymongo import MongoClient
+from constants import MONGO_URI
+
+mongo_client = MongoClient(MONGO_URI)
--- units-vault/app/constants.py
+++ units-vault/app/constants.py
@@ -4,3 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 gRPC_PORT = int(os.getenv("GRPC_PORT"))
+
+
+MONGO_HOST = os.getenv("MONGODB_HOST")
+MONGO_USER = os.getenv("MONGO_INITDB_ROOT_USERNAME")
+MONGO_PASS = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
+MONGO_DATABASE = os.getenv("MONGODB_DATABASE")
+MONGO_URI = f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}:27017/?authSource={MONGO_DATABASE}"
