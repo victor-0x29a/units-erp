69915a4cc9a3b2d38b00251d072dc75054e871ce
victor-0x29a <cartoonbr495@gmail.com>
2024-07-15 14:07:52 -0400

    Chore: applying the tags meta data on application and a custom config for swagger

--- units-vault/main.py
+++ units-vault/main.py
@@ -1,10 +1,16 @@
 import connection # noqa
 from fastapi import FastAPI
+from constants import TAGS_META_DATA
 from controllers.product_controller import router as user_router
 from controllers.batch_controller import router as batch_router
 
 
-app = FastAPI()
+app = FastAPI(
+    swagger_ui_parameters={'syntaxHighlight.theme': 'obsidian'},
+    openapi_tags=TAGS_META_DATA,
+    # If you are viewing this, change the docs_url to otheer uuid4 with 50 characters
+    docs_url='/9c86ffd5-b84e-4fd9-bb48-37c5f6912020'
+)
 
 app.include_router(user_router)
 app.include_router(batch_router)
