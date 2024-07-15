ae1e7ec2346dd6ee345b7c4d57fb1d8f7aa022ce
0x29a <cartoonbr495@gmail.com>
2024-07-15 00:24:29 -0300

    Fix: adjusting the create batch use case and new var added in constants

--- units-vault/main.py
+++ units-vault/main.py
@@ -1,9 +1,13 @@
+import connection # noqa
 from fastapi import FastAPI
 from controllers.product_controller import router as user_router
+from controllers.batch_controller import router as batch_router
+
 
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(batch_router)
 
 
 @app.get("/hello_world")
--- units-vault/use_cases/v1/create_batch.py
+++ units-vault/use_cases/v1/create_batch.py
@@ -11,21 +11,21 @@ class CreateBatch:
         self.__validate()
 
     def __validate(self):
+        # TO DO: VALIDATE THE CNPJ
+
         batch = self.document.objects(reference=self.batch.get('reference', None))
 
         if batch:
             raise AlreadyExists(message="Batch already exists by reference.")
 
-        expiry_date = self.batch.get('expiry_date', None)
-
-        expiry_date = datetime.strptime(expiry_date, "%Y-%m-%d")
-
         current_date = datetime.now()
+
+        expiry_date = self.batch.get('expiry_date', None)
 
         if expiry_date < current_date:
             raise LessThanCurrentDate()
 
-    def start(self):
+    def start(self) -> Batch:
         created_batch = self.document(**self.batch)
 
         created_batch.save()
