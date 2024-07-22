fe8bdce4e83d74b0521628dee8ebbe6fb98f7d17
0x29a <cartoonbr495@gmail.com>
2024-07-22 00:15:19 -0300

    Feat: button component has created

---
+++ units-cashier/src/components/button/Button.tsx
@@ -0,0 +1,21 @@
+import styles from "./Button.module.scss"
+import { IButtonProps } from "./Button.types";
+
+export const Button = ({
+    label,
+    disabled,
+    isLoading,
+    onClick,
+    type = "default",
+    loadingLabel = "Carregando..."
+}: IButtonProps) => {
+    return (
+        <button
+            onClick={onClick}
+            disabled={disabled || isLoading}
+            className={[styles.btn, styles[type]].join(' ')}
+        >
+            {isLoading ? loadingLabel : label}
+        </button>
+    );
+}
