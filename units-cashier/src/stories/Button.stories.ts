2ba1fdbf18ef3fefd5e35929597a30c18f92ce8f
victor-0x29a <cartoonbr495@gmail.com>
2024-07-24 15:27:24 -0400

    Chore: correct import for btn stories and stories has been created for text input

--- units-cashier/src/stories/Button.stories.ts
+++ units-cashier/src/stories/Button.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
-import { Button } from '../components/button';
+import { Button } from '../components/Button';
 
 const meta = {
   title: 'Form/Button',
