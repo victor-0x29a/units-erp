09dae2e956eeceba5440b5acc4ba1dae3568e506
victor-0x29a <cartoonbr495@gmail.com>
2024-07-24 14:30:27 -0400

    Chore: home page has been cleaned

--- units-cashier/src/app/page.tsx
+++ units-cashier/src/app/page.tsx
@@ -1,19 +1,22 @@
 "use client"
 
-import { TextInput, Button } from "@/components";
-import { useState } from "react";
+import { useFormik } from "formik";
+import { Button } from "@/components";
+import { LoginSchema, LoginInitialValues } from "@/schemas/auth/login";
 import styles from "./page.module.scss";
 
 export default function Home() {
-  const [value, setValue] = useState<string>("");
-  const callBack = (field: string) => (value: string) => setValue(value);
+  const formik = useFormik({
+    initialValues: LoginInitialValues,
+    validationSchema: LoginSchema,
+    onSubmit: () => {}
+  })
+
   return (
     <main>
       <div className="box">
-        <form className={styles['login-form']}>
-          <TextInput label="Usuário" onChange={callBack('user')} onBlur={() => { }} value={value} />
-          <TextInput label="Senha" onChange={callBack('password')} onBlur={() => { }} value={value} />
-          <Button label="Entrar" type="success" />
+        <form className={styles['login-form']} onSubmit={formik.handleSubmit}>
+          <Button type="success">Log-in</Button>
         </form>
       </div>
     </main>
