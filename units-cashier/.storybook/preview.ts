250bdc1b0f0bfc1add2dcc95305cf269fe9861cb
victor-0x29a <cartoonbr495@gmail.com>
2024-07-24 15:19:41 -0400

    Chore: storybook has been configured

--- units-cashier/.storybook/preview.ts
+++ units-cashier/.storybook/preview.ts
@@ -1,7 +1,17 @@
 import type { Preview } from "@storybook/react";
+import "../src/app/globals.scss"
 
 const preview: Preview = {
   parameters: {
+    // backgrounds: {
+    //   default: 'black',
+    //   values: [
+    //     {
+    //       name: 'black',
+    //       value: '#000000',
+    //     },
+    //   ],
+    // },
     controls: {
       matchers: {
         color: /(background|color)$/i,
