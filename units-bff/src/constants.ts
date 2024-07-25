df1084b4589ab8ae669f2ba663ee9c23177c0d5d
victor-0x29a <cartoonbr495@gmail.com>
2024-07-25 09:29:01 -0400

    Chore: axios added in project with the loading of units vault var's

--- units-bff/src/constants.ts
+++ units-bff/src/constants.ts
@@ -4,6 +4,11 @@ const environment = process.env.NODE_ENV;
 
 dotenv.config({ path: `./.env.${environment}` });
 
-export const SERVER_PORT = Number(process.env.SERVER_PORT || 4500);
+export const SERVER_PORT: number = Number(process.env.SERVER_PORT || 4500);
 
-export const CAN_LOG = Boolean(Number(process.env.CAN_LOG));
+export const CAN_LOG: boolean = Boolean(Number(process.env.CAN_LOG));
+
+export const UNITS_VAULT_URL: string = process.env.UNITS_VAULT;
+
+export const UNITS_VAULT_USER_AGENT: string =
+  process.env.UNITS_VAULT_USER_AGENT;
