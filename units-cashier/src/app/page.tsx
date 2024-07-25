245afaf2054b62931b60f2aa0f8cea23a2b6ef30
0x29a <cartoonbr495@gmail.com>
2024-07-24 23:44:12 -0300

    Chore: base of login page

--- units-cashier/src/app/page.tsx
+++ units-cashier/src/app/page.tsx
@@ -1,22 +1,54 @@
 'use client';
 
 import { useFormik } from 'formik';
-import { Button } from '@/components';
+import { Button, Column, TextInput } from '@/components';
 import { LoginSchema, LoginInitialValues } from '@/schemas/auth/login';
 import styles from './page.module.scss';
+import Image from 'next/image';
 
 export default function Home() {
   const formik = useFormik({
     initialValues: LoginInitialValues,
     validationSchema: LoginSchema,
-    onSubmit: () => {}
+    onSubmit: () => {},
+    validateOnBlur: false,
+    validateOnChange: false
   });
 
   return (
     <main>
-      <div className="box">
-        <form className={styles['login-form']} onSubmit={formik.handleSubmit}>
-          <Button type="success">Log-in</Button>
+      <div className={styles.box}>
+        <form onSubmit={formik.handleSubmit}>
+          <Image src='/logo.png' alt='Logo' width={200} height={200} />
+          <Column
+            hasError={Boolean(formik.errors.username)}
+            isTouched={formik.touched.username}
+            error={formik.errors.username}
+          >
+            <TextInput
+              label='Usuário'
+              value={formik.values.username}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              name='username'
+              type='text'
+            />
+          </Column>
+          <Column
+            hasError={Boolean(formik.errors.password)}
+            isTouched={formik.touched.password}
+            error={formik.errors.password}
+          >
+            <TextInput
+              label='Senha'
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              name='password'
+              type='password'
+            />
+          </Column>
+          <Button type='submit'>Entrar</Button>
         </form>
       </div>
     </main>
