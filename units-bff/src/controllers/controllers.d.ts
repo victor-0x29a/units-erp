2c87cbf04ef7c8a05f70663c30a9bd6e2881d03f
victor-0x29a <cartoonbr495@gmail.com>
2024-07-26 07:44:04 -0400

    Patch: home controller has added and unused type files are revmoed

--- units-bff/src/controllers/controllers.d.ts
+++
@@ -1,6 +0,0 @@
-import { FastifyReply, FastifyRequest } from "fastify";
-
-export type FastifyHandler = (
-  request: FastifyRequest,
-  reply: FastifyReply
-) => void | Promise<void> | Promise<unknown>;
---
+++ units-bff/src/controllers/home/dto.ts
@@ -0,0 +1,3 @@
+export class SayHelloDTO {
+  name: string;
+}
