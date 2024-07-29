db751c1f49bfaa6d237078784e447dfb5a20e526
victor-0x29a <cartoonbr495@gmail.com>
2024-07-29 14:59:21 -0400

    Feat: specs E2E to products route

---
+++ units-vault/specs/e2e/v1/test_create_product_v1.py
@@ -0,0 +1,79 @@
+from main import app
+import pytest
+from fastapi.testclient import TestClient
+from unittest.mock import MagicMock
+from exceptions import HasWithSameBatch, GreaterThanPrice
+from documents import Product, Batch
+
+client = TestClient(app)
+
+
+class TestCreateProductE2EV1():
+    def test_should_create(self, mocker):
+        magic_batch = MagicMock()
+
+        magic_batch.id = "batch00001"
+
+        mocker.patch.object(Batch, 'objects', return_value=magic_batch)
+
+        mocker.patch.object(Product, 'objects', return_value=None)
+
+        mocker.patch.object(Product, 'save', return_value=True)
+
+        response = client.post("/v1/product", json={
+            "name": "Foo",
+            "price": 3.45,
+            "base_stock": 310,
+            "batch": "batch00002",
+            "type": "construction",
+            "for_use": "for_sale"
+        })
+
+        assert response.status_code == 204
+
+    def test_should_reject_when_already_exists_with_same_batch(eslf, mocker):
+        magic_batch = MagicMock()
+
+        magic_batch.id = "batch00001"
+
+        mocker.patch.object(Batch, 'objects', return_value=magic_batch)
+
+        mocker.patch.object(Product, 'objects', return_value=True)
+
+        mocker.patch.object(Product, 'save', return_value=True)
+
+        with pytest.raises(HasWithSameBatch) as exception:
+            client.post("/v1/product", json={
+                "name": "Foo",
+                "price": 3.45,
+                "base_stock": 310,
+                "batch": "batch00002",
+                "type": "construction",
+                "for_use": "for_sale"
+            })
+
+        assert "Already has product with the same batch." == exception.value.message
+
+    def test_should_reject_when_discount_is_greater_than_price(eslf, mocker):
+        magic_batch = MagicMock()
+
+        magic_batch.id = "batch00001"
+
+        mocker.patch.object(Batch, 'objects', return_value=magic_batch)
+
+        mocker.patch.object(Product, 'objects', return_value=False)
+
+        mocker.patch.object(Product, 'save', return_value=True)
+
+        with pytest.raises(GreaterThanPrice) as exception:
+            client.post("/v1/product", json={
+                "name": "Foo",
+                "price": 3.45,
+                "discount_value": 4.0,
+                "base_stock": 310,
+                "batch": "batch00002",
+                "type": "construction",
+                "for_use": "for_sale"
+            })
+
+        assert "The discount is greater than the price of the product." == exception.value.message
