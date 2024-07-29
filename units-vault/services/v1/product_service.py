3c84edb187db9041eddd88f7ae862d9e99396290
victor-0x29a <cartoonbr495@gmail.com>
2024-07-29 15:56:08 -0400

    Refactor: processing the payload of batch outside of service

--- units-vault/services/v1/product_service.py
+++ units-vault/services/v1/product_service.py
@@ -4,20 +4,8 @@ from use_cases import CreateProductV1
 
 class ProductService:
     def create(self, data={}):
-        data = data.model_dump()
-        create_payload = {
-            **data,
-            "item_type": data.get("type"),
-            "data_type": data.get("for_use"),
-            "stock": data.get("base_stock")
-        }
-
-        create_payload.pop("type")
-        create_payload.pop("for_use")
-        create_payload.pop("base_stock")
-
         CreateProductV1(
             batch_document=Batch,
             product_document=Product,
-            product_data=create_payload
+            product_data=data
         ).start()
