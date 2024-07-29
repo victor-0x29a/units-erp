79c48d7decad83a74d4acb36d06a510db59ff4ad
victor-0x29a <cartoonbr495@gmail.com>
2024-07-29 15:54:45 -0400

    Refactor: processing the payload of product outside of service

--- units-vault/services/v1/batch_service.py
+++ units-vault/services/v1/batch_service.py
@@ -1,22 +1,12 @@
 from documents import Batch
 from use_cases import CreateBatchV1
-from utils import dates
 
 
 class BatchService:
     def create(self, data={}):
-        data = data.model_dump()
-
-        create_payload = {
-            "expiry_date": dates.parse_date(data.get("expiry_date")),
-            "inserction_datetime": dates.get_now(),
-            "supplier_document": data.get("cnpj"),
-            "reference": data.get("ref")
-        }
-
         create_worker = CreateBatchV1(
             batch_document=Batch,
-            data=create_payload
+            data=data
         )
 
         create_worker.start()
