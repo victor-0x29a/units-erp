28062101fd22d2188c8e94d61ebdf99f705158b7
victor-0x29a <cartoonbr495@gmail.com>
2024-07-31 14:44:52 -0400

    Refactor: unecessary injection of dependency has removed

--- units-vault/services/v1/batch_service.py
+++ units-vault/services/v1/batch_service.py
@@ -1,11 +1,9 @@
-from documents import Batch
 from use_cases import CreateBatchV1
 
 
 class BatchService:
     def create(self, data={}):
         create_worker = CreateBatchV1(
-            batch_document=Batch,
             data=data
         )
 
