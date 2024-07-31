7b916d793781d633a29efc719f611bd4f6307b25
victor-0x29a <cartoonbr495@gmail.com>
2024-07-31 14:58:11 -0400

    Refactor: correct method names

--- units-vault/utils/dates.py
+++ units-vault/utils/dates.py
@@ -5,5 +5,9 @@ def get_now():
     return datetime.now().replace(tzinfo=timezone.utc)
 
 
-def parse_date(date: str, format: str = "%d/%m/%Y"):
+def from_str_to_date(date: str, format: str = "%d/%m/%Y"):
     return datetime.strptime(date, format).replace(tzinfo=timezone.utc)
+
+
+def from_date_to_str(date: datetime, format: str = "%d/%m/%Y"):
+    return date.strftime(format)
