7c06060ed19dc7c127323ed0990df365979a1821
victor-0x29a <cartoonbr495@gmail.com>
2024-07-31 15:07:13 -0400

    Refactor: adapting the specs for use a pattern of response and correct validations

--- units-vault/specs/use_cases/test_create_product_v1.py
+++ units-vault/specs/use_cases/test_create_product_v1.py
@@ -1,5 +1,9 @@
+from datetime import timedelta
 from use_cases import CreateProductV1
+from documents import Product, Batch
+from ..fixture import mongo_connection # noqa: F401, E261
 from .helpers.generate_object_id import generate_object_id
+from utils.dates import get_now
 
 
 class TestCreateProductUseCaseV1:
@@ -10,20 +14,15 @@ class TestCreateProductUseCaseV1:
             'discount_value': 1
         }
 
-        mock_product = mocker.patch('documents.Product')
+        mocker.patch.object(Product, 'objects', return_value=None)
 
-        mocker.patch.object(mock_product, 'objects', return_value=None)
-
-        mocker.patch.object(mock_product, 'save', return_value=True)
+        mocker.patch.object(Product, 'save', return_value=True)
 
         product = CreateProductV1(
-            product_document=mock_product,
             product_data=data
         ).start()
 
-        assert product[0]
-
-        assert product[1] == data
+        assert product
 
     def test_should_fail_when_discount_is_greater(self, mocker):
         data = {
@@ -32,15 +31,12 @@ class TestCreateProductUseCaseV1:
             'discount_value': 11
         }
 
-        mock_product = mocker.patch('documents.Product')
+        mocker.patch.object(Product, 'objects', return_value=None)
 
-        mocker.patch.object(mock_product, 'objects', return_value=None)
-
-        mocker.patch.object(mock_product, 'save', return_value=True)
+        mocker.patch.object(Product, 'save', return_value=True)
 
         try:
             CreateProductV1(
-                product_document=mock_product,
                 product_data=data
             ).start()
         except Exception as e:
@@ -53,44 +49,41 @@ class TestCreateProductUseCaseV1:
             'discount_value': 1
         }
 
-        mock_product = mocker.patch('documents.Product')
+        mocker.patch.object(Product, 'objects', return_value=True)
 
-        mocker.patch.object(mock_product, 'objects', return_value=True)
-
-        mocker.patch.object(mock_product, 'save', return_value=True)
+        mocker.patch.object(Product, 'save', return_value=True)
 
         try:
             CreateProductV1(
-                product_document=mock_product,
                 product_data=data
             ).start()
         except Exception as e:
             assert 'batch' in str(e)
 
     def test_should_find_a_batch_when_batch_is_string(self, mocker):
+        batch_reference = "ref123"
+
+        created_batch_id = Batch(
+            supplier_document="123123",
+            reference=batch_reference,
+            expiry_date=get_now() + timedelta(days=1),
+            inserction_datetime=get_now()
+        ).save().id
+
         data = {
-            'batch': '123123',
+            'batch': batch_reference,
             'price': 10,
-            'discount_value': 1
+            'discount_value': 1,
+            'name': 'Foo product',
+            'stock': 10,
+            'item_type': 'Foo type'
         }
 
-        obj_id = generate_object_id()
-
-        mock_product_cls = mocker.MagicMock()
-
-        mock_batch_cls = mocker.MagicMock()
-
-        mock_batch_cls.objects.get.return_value.id = obj_id
-
-        mock_product_cls.objects.return_value = None
-
         product = CreateProductV1(
-            product_document=mock_product_cls,
-            batch_document=mock_batch_cls,
             product_data=data
         ).start()
 
-        assert product[1].get('batch') == obj_id
+        assert product.batch.id == created_batch_id
 
     def test_should_create_with_bar_code(self, mocker):
         data = {
@@ -100,20 +93,15 @@ class TestCreateProductUseCaseV1:
             'bar_code': '123456789012'
         }
 
-        mock_product = mocker.patch('documents.Product')
+        mocker.patch.object(Product, 'objects', return_value=None)
 
-        mocker.patch.object(mock_product, 'objects', return_value=None)
-
-        mocker.patch.object(mock_product, 'save', return_value=True)
+        mocker.patch.object(Product, 'save', return_value=True)
 
         product = CreateProductV1(
-            product_document=mock_product,
             product_data=data
         ).start()
 
-        assert product[0]
-
-        assert product[1] == data
+        assert product
 
     def test_should_create_without_bar_code(self, mocker):
         data = {
@@ -122,17 +110,12 @@ class TestCreateProductUseCaseV1:
             'discount_value': 1
         }
 
-        mock_product = mocker.patch('documents.Product')
+        mocker.patch.object(Product, 'objects', return_value=None)
 
-        mocker.patch.object(mock_product, 'objects', return_value=None)
-
-        mocker.patch.object(mock_product, 'save', return_value=True)
+        mocker.patch.object(Product, 'save', return_value=True)
 
         product = CreateProductV1(
-            product_document=mock_product,
             product_data=data
         ).start()
 
-        assert product[0]
-
-        assert product[1] == data
+        assert product
