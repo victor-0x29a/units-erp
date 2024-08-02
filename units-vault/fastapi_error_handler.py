d03ab3736bd30b8a8772c4440305367c25c4b0ea
victor-0x29a <cartoonbr495@gmail.com>
2024-08-02 08:42:40 -0400

    Chore: validating if is mongoengine error in error handler

--- units-vault/fastapi_error_handler.py
+++ units-vault/fastapi_error_handler.py
@@ -4,12 +4,24 @@ from exceptions import exceptions_code
 
 
 async def unhandled_exceptions(request: Request, exc: Exception):
+    internal_server_error_label = "Internal server error."
+
     try:
         message = exc.message
     except Exception:
-        message = "Internal server error."
+        message = internal_server_error_label
 
-    status_code = exceptions_code.get(exc.code, {}).get("http", 500)
+    try:
+        code = exc.__getattribute__("code")
+    except Exception:
+        code = 500
+
+    is_mongo_exception = exc.__class__.__module__.startswith("mongoengine")
+
+    if is_mongo_exception:
+        message = internal_server_error_label
+
+    status_code = exceptions_code.get(code, {}).get("http", 500)
     return JSONResponse({
         "message": message
     }, status_code=status_code)
