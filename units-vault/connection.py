07269638f8736ce79853c316a722351caaaf01aa
victor-0x29a <cartoonbr495@gmail.com>
2024-08-02 09:29:34 -0400

    Chore: using mongo abstractions instead pure string

--- units-vault/connection.py
+++ units-vault/connection.py
@@ -1,6 +1,11 @@
 from pymongo import MongoClient
 from mongoengine import connect
-from constants import MONGO_URI, MONGO_DATABASE, MONGO_HOST, MONGO_PASS, MONGO_USER
+from constants import (
+    MONGO_URI, MONGO_DATABASE,
+    MONGO_HOST, MONGO_PASS,
+    MONGO_USER, MONGO_AUTHENTICATION_SOURCE,
+    MONGO_UUID_REPRESENTATION
+)
 
 
 mongo_client = MongoClient(MONGO_URI)
@@ -9,6 +14,6 @@ connect(
     host=MONGO_HOST,
     username=MONGO_USER,
     password=MONGO_PASS,
-    authentication_source='admin',
-    uuidRepresentation='standard'
+    authentication_source=MONGO_AUTHENTICATION_SOURCE,
+    uuidRepresentation=MONGO_UUID_REPRESENTATION
 )
