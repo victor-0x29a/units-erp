4693061dfd05a881a9b61301255213d034c8cf75
Victor W. <cartoonbr495@gmail.com>
2024-11-05 14:39:30 -0400

    Chore: attaching the store on batch creation 🧩

--- units-vault/controllers/dto/batch/v1/create_batch.py
+++ units-vault/controllers/dto/batch/v1/create_batch.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 
 
 class CreateBatch(BaseModel):
+    store_unit: int
     cnpj: str
     ref: str
     expiry_date: str
--- units-vault/documents/batch.py
+++ units-vault/documents/batch.py
@@ -1,4 +1,5 @@
-from mongoengine import Document, StringField, DateTimeField, DateField, ValidationError
+from .store import Store
+from mongoengine import Document, StringField, DateTimeField, DateField, ValidationError, ReferenceField
 from pycpfcnpj import cpfcnpj
 
 
@@ -14,3 +15,4 @@ class Batch(Document):
     reference = StringField(required=True, unique=True)
     inserction_datetime = DateTimeField(required=True)
     expiry_date = DateField(required=True)
+    store = ReferenceField(Store, required=True)
