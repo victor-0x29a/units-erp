f3005df6aaa8f469564b1f00dbb1a151ab48480f
Victor W. <cartoonbr495@gmail.com>
2024-11-07 08:43:48 -0400

    Chore: better login page ✏️

--- units-cashier/next.config.mjs
+++ units-cashier/next.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        optimizePackageImports: ['@chakra-ui/react'],
+        optimizePackageImports: ['@chakra-ui/react']
     },
+    images: {
+        remotePatterns: [
+            {
+                hostname: 'img.freepik.com',
+                pathname: '/fotos-gratis/**',
+                protocol: 'https',
+                port: ''
+            }
+        ]
+    }
 };
 
 export default nextConfig;
--- units-cashier/src/app/page.tsx
+++ units-cashier/src/app/page.tsx
@@ -3,9 +3,11 @@
 import { useFormik } from 'formik';
 import { Button } from '@/components/ui/button';
 import { Field } from '@/components/ui/field';
-import { Input, Stack } from '@chakra-ui/react';
+import { Input, Link, Stack } from '@chakra-ui/react';
 import { LoginSchema, LoginInitialValues } from '@/schemas/auth/login';
 import styles from './page.module.scss';
+import { Prose } from '@/components/ui/prose';
+import Image from 'next/image';
 
 export default function Home() {
   const formik = useFormik({
@@ -17,44 +19,56 @@ export default function Home() {
   });
 
   return (
-    <main>
-      <div className={styles.box}>
-        <form onSubmit={formik.handleSubmit}>
-            <Stack gap="5" maxW={'sm'}>
-                <Field
-                    label='Usuário'
-                    invalid={Boolean(formik.errors.username)}
-                    errorText={formik.errors.username}>
-                    <Input
-                        value={formik.values.username}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        name='username'
-                        type='text'
-                    />
-                </Field>
-                <Field
-                    label='Senha'
-                    invalid={Boolean(formik.errors.password)}
-                    errorText={formik.errors.password}>
-                    <Input
-                        value={formik.values.password}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        name='password'
-                        type='password'
-                    />
-                </Field>
-                <Button
-                    type='submit'
-                    variant={'solid'}
-                    colorPalette={'orange'}
-                    width={'100%'}>
-                    Entrar
-                </Button>
-            </Stack>
-        </form>
-      </div>
+    <main className={styles.container}>
+        <div className={styles.side}>
+            <Prose size={'lg'}>
+                <span>
+                    <Image src={'https://img.freepik.com/fotos-gratis/retrato-de-um-mulher-negocio-trabalhar-laptop_1303-9733.jpg'} alt={'Logo'} width={600} height={600} />
+                </span>
+                <h1>Construindo a tecnologia</h1>
+                <p>
+                    Venha conhecer o <Link  href='https://github.com/victor-0x29a/units-erp'>melhor ERP do mercado</Link> para o seu varejo.
+                    Aumente a eficiência e impulsione suas vendas com nossa solução inovadora!
+                </p>
+            </Prose>
+        </div>
+        <div className={styles.side}>
+            <form onSubmit={formik.handleSubmit} className={styles.form}>
+                <Stack gap="5" maxW={'sm'}>
+                    <Field
+                        label='Usuário'
+                        invalid={Boolean(formik.errors.username)}
+                        errorText={formik.errors.username}>
+                        <Input
+                            value={formik.values.username}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            name='username'
+                            type='text'
+                        />
+                    </Field>
+                    <Field
+                        label='Senha'
+                        invalid={Boolean(formik.errors.password)}
+                        errorText={formik.errors.password}>
+                        <Input
+                            value={formik.values.password}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            name='password'
+                            type='password'
+                        />
+                    </Field>
+                    <Button
+                        type='submit'
+                        variant={'solid'}
+                        colorPalette={'orange'}
+                        width={'100%'}>
+                        Entrar
+                    </Button>
+                </Stack>
+            </form>
+        </div>
     </main>
   );
 }
