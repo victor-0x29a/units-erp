af4ee8e4a4ccc724aab85188da70c6702621f9b9
Victor W. <cartoonbr495@gmail.com>
2024-11-08 00:04:01 -0300

    Fix: jest config after paths on tsconfig 🩹

--- units-bff/jest.config.ts
+++ units-bff/jest.config.ts
@@ -90,7 +90,10 @@ const config: Config = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '@constants': '<rootDir>/src/constants.ts',
+    '@module': '<rootDir>/src/modules/'
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
