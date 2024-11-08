fc5923c7b5b2a2f779c88790444457365ca20b72
Victor W. <cartoonbr495@gmail.com>
2024-11-08 13:28:32 -0400

    Feat: employee entity has been created 🧬

--- units-vault/documents/__init__.py
+++ units-vault/documents/__init__.py
@@ -1,3 +1,4 @@
 from .batch import Batch
 from .products import Product
 from .store import Store
+from .employee import Employee
---
+++ units-vault/documents/employee.py
@@ -0,0 +1,24 @@
+from mongoengine import Document, StringField, ReferenceField
+import cpfcnpj
+from .store import Store
+from docs_constants import EMPLOYEE_ROLES
+from exceptions import ValidationError
+
+
+roles_available = [role for role in EMPLOYEE_ROLES.values()]
+
+
+def _is_valid_cpf(value):
+    is_valid_cnpj = cpfcnpj.validate(value)
+
+    if value and not is_valid_cnpj:
+        raise ValidationError('The employee doc should be valid.')
+
+
+class Employee(Document):
+    name = StringField(required=True, max_length=60)
+    document = StringField(required=True, validation=_is_valid_cpf)
+    unit = ReferenceField(Store, required=True)
+    username = StringField(required=True, max_length=30)
+    password = StringField(max_length=120, default=None)
+    role = StringField(required=True, choices=roles_available)
