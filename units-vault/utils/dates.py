67e1dc17dc93e50d1874898f53bea5f573e60a24
Victor W. <cartoonbr495@gmail.com>
2024-11-11 15:00:55 -0400

    Fix: cannot importing UTC from datetime 🩹

--- units-vault/utils/dates.py
+++ units-vault/utils/dates.py
@@ -1,12 +1,12 @@
-from datetime import datetime, timezone, UTC
+import datetime
 
 
 def get_now():
-    return datetime.now(UTC).replace(tzinfo=timezone.utc)
+    return datetime.datetime.now(datetime.UTC).replace(tzinfo=datetime.timezone.utc)
 
 
 def from_str_to_date(date: str, format: str = "%d/%m/%Y"):
-    return datetime.strptime(date, format).replace(tzinfo=timezone.utc)
+    return datetime.datetime.strptime(date, format).replace(tzinfo=datetime.timezone.utc)
 
 
 def from_date_to_str(date: datetime, format: str = "%d/%m/%Y"):
