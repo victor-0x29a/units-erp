7cfa3b7859a369251f2e9be478eed5ed8d01a37b
Victor W. <cartoonbr495@gmail.com>
2024-11-13 00:15:09 -0300

    Feat: employee service has been created ⚙️

---
+++ units-bff/src/services/employee/employee.service.ts
@@ -0,0 +1,21 @@
+import { EmployeeLoginPayloadRequest } from "../../interfaces";
+import { privateDomains } from "../../core";
+import { ExternalError, InvalidCredentials } from "../../exceptions";
+import { ErrorResponse } from "../../core/types/global/response";
+const { employeeDomain } = privateDomains;
+
+
+class EmployeeService {
+  public async login(loginPayload: EmployeeLoginPayloadRequest) {
+    return employeeDomain.login(loginPayload)
+      .then((employeeToken) => employeeToken)
+      .catch((error: ErrorResponse) => {
+        if ([404, 401].includes(error.statusCode)) {
+          return Promise.reject(new InvalidCredentials());
+        }
+        return Promise.reject(new ExternalError());
+      });
+  }
+}
+
+export { EmployeeService };
