e80b27ada963ef3adb6c8b21f6e41b61caa9f73d
Victor W. <cartoonbr495@gmail.com>
2024-11-12 22:53:02 -0300

    Fix: specs e2e doesnt stopping 🩹

--- units-bff/src/index.ts
+++ units-bff/src/index.ts
@@ -1,20 +1,6 @@
-import Fastify, { FastifyInstance } from "fastify";
-import controllers from './controllers';
-import { errorHandler } from './middlewares';
-import { CAN_LOG, SERVER_PORT } from "./constants";
+import { createServer } from './fastify';
+import { SERVER_PORT } from './constants';
 
-export const createServer = (): FastifyInstance => {
-  const fastify = Fastify({
-    logger: CAN_LOG,
-  }) as FastifyInstance;
-
-  controllers.forEach((Controller) => {
-    new Controller(fastify);
-  });
-
-  fastify.setErrorHandler(errorHandler);
-
-  return fastify;
-};
-
-createServer().listen(SERVER_PORT);
+createServer().listen({
+  port: SERVER_PORT
+});
