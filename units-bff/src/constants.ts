ff89f16a32206a04b57e3202f0f9b7df10d843c4
Victor W. <cartoonbr495@gmail.com>
2024-11-14 08:15:39 -0400

    Feat: docker compose for mysql  ⚓📦

--- units-bff/src/constants.ts
+++ units-bff/src/constants.ts
@@ -2,6 +2,14 @@ import dotenv from "dotenv";
 
 const environment = process.env.NODE_ENV;
 
+dotenv.config();
+
+export const MYSQL_USER: string = process.env.MYSQL_USER;
+
+export const MYSQL_PASSWORD: string = process.env.MYSQL_ROOT_PASSWORD;
+
+export const MYSQL_HOSTNAME: string = process.env.MYSQL_HOSTNAME;
+
 dotenv.config({ path: `./.env.${environment}` });
 
 export const SERVER_PORT: number = Number(process.env.SERVER_PORT || 4500);
