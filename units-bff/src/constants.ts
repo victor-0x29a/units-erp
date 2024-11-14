2cc7079bc0d1b08c5ed64f909c813b158fbbcb12
Victor W. <cartoonbr495@gmail.com>
2024-11-14 13:10:59 -0400

    Patch: mysql credentials has been added & docker compose has been fixed  🔑

--- units-bff/src/constants.ts
+++ units-bff/src/constants.ts
@@ -4,13 +4,17 @@ const environment = process.env.NODE_ENV;
 
 dotenv.config();
 
-export const MYSQL_USER: string = process.env.MYSQL_USER;
-
 export const MYSQL_PASSWORD: string = process.env.MYSQL_ROOT_PASSWORD;
 
 export const MYSQL_HOSTNAME: string = process.env.MYSQL_HOSTNAME;
 
+export const MYSQL_DATABASE: string = process.env.MYSQL_DATABASE;
+
+export const MYSQL_PORT: number = Number(process.env.MYSQL_PORT);
+
 dotenv.config({ path: `./.env.${environment}` });
+
+export const MYSQL_USER: string = process.env.MYSQL_USER;
 
 export const SERVER_PORT: number = Number(process.env.SERVER_PORT || 4500);
 
