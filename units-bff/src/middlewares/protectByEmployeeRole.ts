2357dc4ccb044404e9c0a7e89df99849b6f5d695
Victor W. <cartoonbr495@gmail.com>
2024-11-18 15:37:33 -0400

    Feat: protect by emploee role middleware has been created 🔓🔐🏷️

---
+++ units-bff/src/middlewares/protectByEmployeeRole.ts
@@ -0,0 +1,36 @@
+import { FastifyRequest, FastifyReply, DoneFuncWithErrOrRes } from "fastify";
+import { InvalidAuthorization, ExpiredAuthorization, InternalError, InsufficientPermissions } from "../exceptions";
+import { Roles } from "../types/employee";
+import { SignatureManager } from "../external";
+
+export const protectByEmployeeRole = (requiredPermissions: Roles[]) => {
+  const signatureManager = new SignatureManager();
+
+  return async (request: FastifyRequest, _reply: FastifyReply, done: DoneFuncWithErrOrRes) => {
+    const { authorization: authorizationToken } = request.headers;
+
+    if (!authorizationToken) {
+      throw new InvalidAuthorization();
+    }
+
+    return signatureManager.checkIsValid(authorizationToken)
+      .then(() => {
+        const decodedToken = signatureManager.decode(authorizationToken);
+
+        const isAdmin = decodedToken.employeeRole === "ADMIN";
+
+        if (isAdmin) {
+          return done();
+        }
+
+        if (!requiredPermissions.includes(decodedToken.employeeRole)) {
+          throw new InsufficientPermissions(requiredPermissions);
+        }
+
+        return done();
+      })
+      .catch((error: InternalError | InvalidAuthorization | ExpiredAuthorization) => {
+        throw error;
+      });
+  };
+};
