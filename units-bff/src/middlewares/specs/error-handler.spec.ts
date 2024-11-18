dded9d5e85b4db9997182c47aa421c0765b78c6e
Victor W. <cartoonbr495@gmail.com>
2024-11-18 15:48:48 -0400

    Chore: max coverage of error handler middleware 🗿

---
+++ units-bff/src/middlewares/specs/error-handler.spec.ts
@@ -0,0 +1,24 @@
+import { FastifyError, FastifyReply, FastifyRequest } from "fastify";
+import { errorHandler } from "../error-handler";
+import { MountErrorResponse } from "../../utils";
+
+test('should test when have multiple errors', () => {
+  const reply = {
+    status: jest.fn().mockReturnValue({
+      send: jest.fn(),
+    }),
+  } as unknown as FastifyReply;
+
+  const error = {
+    errors: ['a', 'b']
+  } as unknown as FastifyError & {
+    errors: string[];
+  };
+
+  const request = {} as unknown as FastifyRequest;
+
+  errorHandler(error, request, reply);
+
+  expect(reply.status).toHaveBeenCalledWith(422);
+  expect(reply.status(200).send).toHaveBeenCalledWith(MountErrorResponse("MULTIPLE_ERRORS", "MULTIPLE_ERRORS", ["a", "b"]));
+});
