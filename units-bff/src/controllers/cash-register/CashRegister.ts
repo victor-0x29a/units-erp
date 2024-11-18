48f4571c421ddbdbbef377c32f756e29c7ab5722
Victor W. <cartoonbr495@gmail.com>
2024-11-18 16:03:11 -0400

    Feat: CashRegister controller with create register route ⚙️🔀📦

--- units-bff/src/controllers/cash-register/CashRegister.ts
+++ units-bff/src/controllers/cash-register/CashRegister.ts
@@ -1,26 +1,46 @@
-import { FastifyInstance/*, FastifyRequest, FastifyReply */ } from "fastify";
-// import { Builder } from "../Builder";
-// import { protectByEmployeeRole } from "../../middlewares";
-// import { ValidatorCompiler } from "../../utils";
+import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
+import { Builder } from "../Builder";
+import { protectByEmployeeRole } from "../../middlewares";
+import { MountErrorResponse, ValidatorCompiler } from "../../utils";
 import { CashRegisterService } from "../../services/cash-register/cash-register.service";
 import { CashRegisterClock } from "../../entity";
+import { SignatureManager } from "../../external";
+import { FastifyError } from "../../exceptions/FastifyError";
 
 class CashRegister {
   service: CashRegisterService;
+  signatureManager: SignatureManager;
 
   constructor(private readonly fastify: FastifyInstance) {
     this.service = new CashRegisterService(CashRegisterClock);
+    this.signatureManager = new SignatureManager();
 
-    // const ControllerConstructor = new Builder(fastify);
+    const ControllerConstructor = new Builder(fastify);
 
-    // ControllerConstructor.createRoute("post")("/", {
-    //   handler: this.createRegister,
-    //   validatorCompiler: ValidatorCompiler,
-    //   onRequest: [protectByEmployeeRole(["OPERATOR"])],
-    // });
+    ControllerConstructor.createRoute("post")("/", {
+      handler: this.createRegister,
+      validatorCompiler: ValidatorCompiler,
+      onRequest: [protectByEmployeeRole(["OPERATOR"], this.signatureManager)],
+    });
   }
-  // private createRegister = async (request: FastifyRequest, reply: FastifyReply) => {
-  // };
+  private createRegister = async (request: FastifyRequest, reply: FastifyReply) => {
+    const {
+      employeeDocument
+    } = this
+      .signatureManager
+      .decode(request.headers.authorization);
+
+    return this.service
+      .createClockRegister(employeeDocument)
+      .then(() => reply.status(204).send())
+      .catch((error: FastifyError) => {
+        return reply
+          .status(error.statusCode)
+          .send(
+            MountErrorResponse(error.code, error.message, error.errors)
+          );
+      });
+  };
 }
 
 export { CashRegister };
--- units-bff/src/controllers/index.ts
+++ units-bff/src/controllers/index.ts
@@ -1,9 +1,11 @@
 import { Home } from "./home";
 import { Employee } from "./employee";
+import { CashRegister } from "./cash-register/CashRegister";
 
 const controllers = [
   [Home, "/"],
-  [Employee, "/v1/employee"]
+  [Employee, "/v1/employee"],
+  [CashRegister, "/v1/cash-register"],
 ] as const;
 
 export default controllers;
