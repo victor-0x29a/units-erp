e6061e417f2f8eac76ab1a499e01a141d6b45f6d
Victor W. <cartoonbr495@gmail.com>
2024-11-19 10:35:10 -0400

    Feat: startOfDay util 📦

--- units-bff/src/utils/index.ts
+++ units-bff/src/utils/index.ts
@@ -2,3 +2,5 @@ export * from './ValidatorCompiler';
 export * from './MountErrorResponse';
 export * from './getNow';
 export * from './getByTimestamp';
+export * from './startOfDay';
+export * from './endOfDay';
