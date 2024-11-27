3bf8a8a24771fc82f0d2533dab1741eae133b10a
victor-0x29a <cartoonbr495@gmail.com>
2024-11-27 15:13:51 -0300

    Patch: create method for product repository ⚙️

--- units-vault/repositories/product_repository.py
+++ units-vault/repositories/product_repository.py
@@ -1,5 +1,6 @@
+from mongoengine import ObjectIdField
 from documents import Product
-from exceptions import MissingParam, MissingDoc
+from exceptions import MissingParam, MissingDoc, HasWithSameBatch, GreaterThanPrice, AlreadyExists
 
 
 class ProductRepository:
@@ -22,3 +23,39 @@ class ProductRepository:
             raise MissingDoc("Product not found.")
 
         return product
+
+    def create(self, data: dict) -> Product:
+        self.__check_if_has_already_exists_by_batch_and_bar_code(
+            batch_pk=data['batch'],
+            bar_code=data['bar_code']
+        )
+
+        self.__validate_discount_value(
+            discount_value=data.get('discount_value'),
+            price=data.get('price')
+        )
+
+        product = self.Product(**data)
+
+        product.save()
+
+        return product
+
+    def __validate_discount_value(self, discount_value: float, price: float) -> None:
+        if discount_value > price:
+            raise GreaterThanPrice()
+
+    def __check_if_has_already_exists_by_batch_and_bar_code(
+        self,
+        batch_pk: ObjectIdField,
+        bar_code: str
+    ) -> bool:
+        product_by_batch = self.Product.objects(batch=batch_pk)
+
+        if product_by_batch:
+            raise HasWithSameBatch()
+
+        product_by_bar_code = self.Product.objects(bar_code=bar_code)
+
+        if product_by_bar_code:
+            raise AlreadyExists('Product already exists by bar code.')
