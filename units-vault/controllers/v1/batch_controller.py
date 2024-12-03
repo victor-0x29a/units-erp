1127b70f7699cd702507b0d0c51cb509d0b3e212
victor-0x29a <cartoonbr495@gmail.com>
2024-12-03 15:23:49 -0400

    Chore: protecting the route of batch creation 🛡️

--- units-vault/controllers/v1/batch_controller.py
+++ units-vault/controllers/v1/batch_controller.py
@@ -1,13 +1,26 @@
-from fastapi import APIRouter
-from ..dto import CreateBatchV1
+from fastapi import APIRouter, Depends
+from middlewares import create_auth_middleware
 from services.v1.batch_service import BatchService
 from utils.dates import from_str_to_date, get_now
-
+from docs_constants import EMPLOYEE_ROLES
+from ..dto import CreateBatchV1
 
 router = APIRouter(prefix='/v1/batch')
 
 
-@router.post('/', status_code=204, tags=['batch'])
+@router.post(
+    '/',
+    status_code=204,
+    tags=['batch'],
+    dependencies=[
+        Depends(
+            create_auth_middleware(
+                enabled_roles=[
+                    EMPLOYEE_ROLES['inventor'],
+                    EMPLOYEE_ROLES['financial']
+                ]
+            ))
+    ])
 def create_batch(payload: CreateBatchV1):
     data = payload.model_dump()
 
