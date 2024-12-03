3362d2e7657d82a159a142a8888f20f3426d2ca0
victor-0x29a <cartoonbr495@gmail.com>
2024-12-03 15:26:01 -0400

    Chore: protection added in product creation route 🛡️

--- units-vault/controllers/v1/product_controller.py
+++ units-vault/controllers/v1/product_controller.py
@@ -1,12 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from middlewares import create_auth_middleware
 from services.v1.product_service import ProductService
+from docs_constants import EMPLOYEE_ROLES
 from ..dto import CreateProductV1
 
 
 router = APIRouter(prefix="/v1/product")
 
 
-@router.post("/", status_code=204, tags=['product'])
+@router.post(
+    "/",
+    status_code=204,
+    tags=['product'],
+    dependencies=[
+        Depends(
+            create_auth_middleware(
+                enabled_roles=[
+                    EMPLOYEE_ROLES['inventor'],
+                    EMPLOYEE_ROLES['financial']
+                ]
+            ))
+    ])
 def create_product(payload: CreateProductV1):
     data = payload.model_dump()
 
