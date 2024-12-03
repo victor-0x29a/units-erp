bb6763d3e2b9a47f31df47d9da1e1a7604067693
victor-0x29a <cartoonbr495@gmail.com>
2024-12-03 15:30:04 -0400

    Chore: protection added in creation, atualization, deletion and visualization route of a store 🛡️

--- units-vault/controllers/v1/store_controller.py
+++ units-vault/controllers/v1/store_controller.py
@@ -1,12 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from middlewares import create_auth_middleware
 from services.v1.store_service import StoreService as StoreServiceV1
+from docs_constants import EMPLOYEE_ROLES
 from ..dto import CreateStoreV1, UpdateStoreV1
-
 
 router = APIRouter(prefix="/v1/store")
 
 
-@router.post("/", status_code=204, tags=['store'])
+@router.post(
+    "/",
+    status_code=204,
+    tags=['store'],
+    dependencies=[
+        Depends(
+            create_auth_middleware(
+                enabled_roles=[
+                    EMPLOYEE_ROLES['admin']
+                ]
+            )
+        )
+    ])
 def create_store(payload: CreateStoreV1):
     data = payload.model_dump()
 
@@ -17,14 +30,31 @@ def create_store(payload: CreateStoreV1):
     service.create()
 
 
-@router.delete("/{unit}", status_code=204, tags=['store'])
+@router.delete(
+    "/{unit}",
+    status_code=204,
+    tags=['store'],
+    dependencies=[
+        Depends(
+            create_auth_middleware(
+                enabled_roles=[
+                    EMPLOYEE_ROLES['admin']
+                ]
+            )
+        )
+    ])
 def delete_store(unit: int):
     service = StoreServiceV1()
 
     service.delete(unit=int(unit))
 
 
-@router.get("/{unit}", tags=['store'])
+@router.get(
+    "/{unit}",
+    tags=['store'],
+    dependencies=[
+        Depends(create_auth_middleware())
+    ])
 def get_store(unit: int):
     service = StoreServiceV1()
 
@@ -37,7 +67,19 @@ def get_store(unit: int):
     return parsed_store
 
 
-@router.put("/{unit}", status_code=204, tags=['store'])
+@router.put(
+    "/{unit}",
+    status_code=204,
+    tags=['store'],
+    dependencies=[
+        Depends(
+            create_auth_middleware(
+                enabled_roles=[
+                    EMPLOYEE_ROLES['admin']
+                ]
+            )
+        )
+    ])
 def update_store(unit: int, payload: UpdateStoreV1):
     data = payload.model_dump()
 
