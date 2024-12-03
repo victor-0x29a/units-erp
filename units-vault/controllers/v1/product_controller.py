b420e3d8b351369eb34c5f98e1d2ff0668f4ddee
victor-0x29a <cartoonbr495@gmail.com>
2024-12-03 15:47:13 -0400

    Chore: delete route for product was added 📦

--- units-vault/controllers/v1/product_controller.py
+++ units-vault/controllers/v1/product_controller.py
@@ -7,6 +7,8 @@ from ..dto import CreateProductV1
 
 router = APIRouter(prefix="/v1/product")
 
+service = ProductService()
+
 
 @router.post(
     "/",
@@ -35,6 +37,21 @@ def create_product(payload: CreateProductV1):
     create_payload.pop("for_use")
     create_payload.pop("base_stock")
 
-    service = ProductService()
-
     service.create(create_payload)
+
+
+@router.delete(
+    "/{bar_code}",
+    status_code=204,
+    tags=['product'],
+    dependencies=[
+        Depends(
+            create_auth_middleware(
+                enabled_roles=[
+                    EMPLOYEE_ROLES['inventor'],
+                    EMPLOYEE_ROLES['financial']
+                ]
+            ))
+    ])
+def delete(bar_code: int):
+    service.delete(bar_code=str(bar_code))
