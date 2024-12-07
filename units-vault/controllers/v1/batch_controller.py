edaf846c9099d0e4fb8ee778cfeec4681a646c91
victor-0x29a <cartoonbr495@gmail.com>
2024-12-07 01:03:03 -0400

    Feat: GET /v1/batch/:referece 📦

--- units-vault/controllers/v1/batch_controller.py
+++ units-vault/controllers/v1/batch_controller.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from middlewares import create_auth_middleware
 from services.v1.batch_service import BatchService
+from services.v1.store_service import StoreService
 from utils.dates import from_str_to_date, get_now
 from docs_constants import EMPLOYEE_ROLES
 from ..dto import CreateBatchV1
@@ -8,6 +9,7 @@ from ..dto import CreateBatchV1
 router = APIRouter(prefix='/v1/batch')
 
 service = BatchService()
+store_service = StoreService()
 
 
 @router.post(
@@ -52,3 +54,30 @@ def create_batch(payload: CreateBatchV1):
     ])
 def delete(reference: str):
     service.delete(reference=reference)
+
+
+@router.get(
+    '/{reference}',
+    tags=['batch'],
+    dependencies=[
+        Depends(
+            create_auth_middleware(
+                enabled_roles=[
+                    EMPLOYEE_ROLES['inventor'],
+                    EMPLOYEE_ROLES['financial'],
+                    EMPLOYEE_ROLES['operator']
+                ]
+            ))
+    ])
+def get(reference: str):
+    batch_data = service.get(reference=reference)
+    print(batch_data)
+    store_unit = store_service.get(filter={"id": batch_data.store.id}).unit
+
+    return {
+        "reference": batch_data.reference,
+        "inserction_datetime": batch_data.inserction_datetime,
+        "expiry_date": batch_data.expiry_date,
+        "store_unit": store_unit,
+        "cnpj": batch_data.supplier_document
+    }
